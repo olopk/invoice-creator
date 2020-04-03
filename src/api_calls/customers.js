1683c7cfc6a1ecaec9a8f5e14c3e64e45b09bf18
olopk <olekwojas@gmail.com>
2020-04-03 21:01:39 +0200

    finished api calls for customers crud.

--- src/api_calls/customers.js
+++ src/api_calls/customers.js
@@ -1,59 +1,48 @@
-import axios from 'axios';
+import axios from '../axiosInstance';
 //CUSTOMERS OPERATIONS
 
 // GET ALL CUSTOMERS
 export const fetch_customers = () => {
-    // return axios.get('http://127.0.0.1:8080/customers')
-    //         .then(response => {
-    //            let data = response.data.map((customer, index) => {
-    //                 const date = customer.date;
-    //                 const newDate = date.slice(0, date.length - 14);
-                
-    //                 return {
-    //                    ...customer,
-    //                    date: newDate,
-    //                    key: index
-    //                }
-    //            })
-    //            return data  
-    //         })
+    const graphqlQuery = {
+        query: `{ getCustomers{ _id name nip city street }}`
+    }
+    return axios.post('/graphql', JSON.stringify(graphqlQuery))
+    .then(response =>{
+        const data = response.data.data.getCustomers.map((customer, index) => {
+            return{ ...customer, key: index}
+        })
+        return {data: data}})
+    .catch(err => {return {error: err}})
 }
-
 // GET SINGLE CUSTOMER
-// export const fetch_customer = (id) => {}
-
+export const fetch_single_customer = (id) => {
+    const graphqlQuery = {
+        query: ` 
+            query FetchSingleCustomer($id: String!){
+                getCustomer(id: $id){ _id name nip city street }}
+        `,
+        variables: { id: id }
+    }
+    return axios.post('/graphql', JSON.stringify(graphqlQuery))
+    .then(result => {return {data: result}})
+    .catch(err => {return {error: err}})
+}
 // SAVE CUSTOMER (NEW & EXISTING)
-export const save_customer = (customer, products, editing) => {
-    // eslint-disable-next-line
-    const sentData = {
-        customer: {
-            ...customer
-        },
-        products: {
-            ...products
+export const save_customer = (customer, editing) => {
+    const graphqlQuery = {
+        query: `
+                mutation CreateNewCustomer($customer: CustomerInputData!){
+                    addCustomer(customerInput: $customer}){message}
+                }
+        `,
+        variables: {
+            customer: {...customer},
         }
     }
-    // we change a bit the method when editing the record in the DB. so we check if we editing.
-    if(editing){
-        //do smth
-    }else{
-        //do smth
-    }
-    
-    // some magic with the POST sending to the server.
-    return new Promise(resolve =>{
-        setTimeout(()=>{
-            resolve({
-                message: 'customer saved successfully',
-                status: 201
-            })
-        },2000)
-    })
 }
 
-// DELETE CUSTOME
+// DELETE CUSTOMER
 export const delete_customer = (id) => {
-    // even more magic with the DELETE call send to the server
     
 }
 
