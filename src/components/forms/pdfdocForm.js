0f3be62f28d015e3dd7b9f99c2e4f01190833e08
olopk <olekwojas@gmail.com>
2020-04-29 22:38:02 +0200

    Huge commit. PdfCreator implemented.

--- src/components/forms/pdfdocForm.js
+++ src/components/forms/pdfdocForm.js
@@ -3,28 +3,32 @@ import pdfFonts from "pdfmake/build/vfs_fonts";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 const createPDF = (props) => {
+	
+	const {invoice_nr, total_price, date, customer_nip, customer_city, customer_street, customer_name, order } = props;
 
-// , total_price, date, customer_nip, customer_city, customer_street, customer_name, order
-
-// name: el.product,
-// unit: 'szt.',
-// quantity: el.quantity,
-// price: el.price,
-// total_price: el.total_price
+	const items = order.map((el, index) => {
+		return [
+			{text: `${index +1}`, style: 'tableRow'},{text: `${el.product}`},
+			{text: `szt. `, style: 'tableRow'},{text: `${el.quantity}`, style: 'tableRow'},
+			{text: `${el.price}`, style: 'tableRow'},{text: `23%`, style: 'tableRow'},
+			{text: ``, style: 'tableRow'},{text: `${el.total_price}`, style: 'tableRow'}
+		]
+	})
 
 	var dd = {
 		content: [
 			{
 				columns: [
-					{   image: 'sampleImage.jpg', width: 200 }
+					// {   image: `sampleImage.jpg`, width: 200 }
+					{text: 'tu bedzie logo'}
 					,
 					{
 						table: {
 						widths: ["*"],
 						body: [
-							[{text: 'Faktura nr '}],
-							[{text: 'Data wystawienia: 2020-04-20'}],
-							[{text: 'Termin płatności: 2020-04-27'}],
+							[{text: `Faktura nr ${invoice_nr}`}],
+							[{text: `Data wystawienia: ${date._i}`}],
+							[{text: `Termin płatności: ${date._i}`}],
 							]
 						}
 					},
@@ -33,20 +37,20 @@ const createPDF = (props) => {
 			{
 				columns: [
 					[
-						{	text: 'Sprzedawca', style: 'header' },
-						{	text: 'Przedsiębiorstwo Wielobranżowe OpenIT Leszek Kaźmierczak' },
-						{	text: 'os. Witosa 18/7' },
-						{	text: '77-300 Człuchów' },
-						{	text: 'NIP: 8431548646' },
-						{	text: 'mBank' },
-						{	text: '81 1140 2004 0000 3702 7042 3504' },
+						{	text: `Sprzedawca`, style: `header` },
+						{	text: `OPTYK Barbara Pałosz` },
+						{	text: `os. Długosza 37` },
+						{	text: `77-300 Człuchów` },
+						{	text: `NIP: 843-15-20-760` },
+						{	text: `PKOBP` },
+						{	text: `8888 8888 8888 8888 8888 8888 ` },
 					],
 					[
-						{	text: 'Nabywca', style: 'header' },
-						{	text: 'Przedsiębiorstwo Wielobranżowe OpenIT Leszek Kaźmierczak' },
-						{	text: 'os. Witosa 18/7' },
-						{	text: '77-300 Człuchów' },
-						{	text: 'NIP: 8431548646' },
+						{	text: `Nabywca`, style: `header` },
+						{	text: `${customer_name}` },
+						{	text: `${customer_street}` },
+						{	text: `${customer_city}` },
+						{	text: `NIP: ${customer_nip}` },
 					]
 				]
 				,
@@ -57,8 +61,8 @@ const createPDF = (props) => {
 				table: {
 					widths: [15, "*", 20, 20, 40,30,60, 60],   
 					body: [
-						[{text: 'Lp.', style: 'tableHeader'},{text: 'Nazwa', style: 'tableHeader'},{text: 'Jedn.', style: 'tableHeader'},{text: 'Ilość', style: 'tableHeader'},{text: 'Cena netto', style: 'tableHeader'},{text: 'Stawka', style: 'tableHeader'},{text: 'Wartość netto', style: 'tableHeader'},{text: 'Wartość brutto', style: 'tableHeader'}],
-						[{text: '1', style: 'tableRow'},{text: 'Okulary'},{text: 'szt.', style: 'tableRow'},{text: '2', style: 'tableRow'},{text: '15', style: 'tableRow'},{text: '23%', style: 'tableRow'},{text: '15', style: 'tableRow'},{text: '55', style: 'tableRow'}],
+						[{text: `Lp.`, style: 'tableHeader'},{text: `Nazwa`, style: 'tableHeader'},{text: `Jedn.`, style: 'tableHeader'},{text: `Ilość`, style: 'tableHeader'},{text: `Cena netto`, style: 'tableHeader'},{text: `Stawka`, style: 'tableHeader'},{text: `Wartość netto`, style: 'tableHeader'},{text: `Wartość brutto`, style: 'tableHeader'}],
+						...items
 					]
 				}
 			},
@@ -69,8 +73,8 @@ const createPDF = (props) => {
 					 //   widths: ["*"],
 						body: [
 							[{text: 'Stawka VAT', style: 'tableHeaderSM'},{text: 'Wartość netto', style: 'tableHeaderSM'},{text: 'Kwota VAT', style: 'tableHeaderSM'},{text: 'Wartość brutto', style: 'tableHeaderSM'}],
-							[{text: '23%', style: 'tableRowSM'},{text: '30', style: 'tableRowSM'},{text: '25', style: 'tableRowSM'},{text: '55', style: 'tableRowSM'}],
-							[{text: 'Razem', style: 'tableRowSM'},{text: '30c', style: 'tableRowSM'},{text: '25', style: 'tableRowSM'},{text: '55', style: 'tableRowSM'}],
+							[{text: `23%`, style: 'tableRowSM'},{text: `30`, style: 'tableRowSM'},{text: `25`, style: 'tableRowSM'},{text: `55`, style: 'tableRowSM'}],
+							[{text: 'Razem', style: 'tableRowSM'},{text: `30c`, style: 'tableRowSM'},{text: `25`, style: 'tableRowSM'},{text: `${total_price}`, style: 'tableRowSM'}],
 							]
 						}
 					},
@@ -83,15 +87,15 @@ const createPDF = (props) => {
 								{text: 'Razem' }  
 							 ],
 							 [
-								{text: '55', alignment: 'right' },
-								{text: '0', alignment: 'right' },
-								{text: '0', alignment: 'right' }  
+								{text: `55`, alignment: 'right' },
+								{text: `0`, alignment: 'right' },
+								{text: `0`, alignment: 'right' }  
 							 ]
 							]
 				 
 						 },
 					 {text: 'Słownie', alignment: 'right'},
-					 {text: 'tysiac pincet sto dziewiecset', alignment: 'right'}
+					 {text: `tysiac pincet sto dziewiecset`, alignment: 'right'}
 						
 					],
 				],
